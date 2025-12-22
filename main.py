f88a9044d22b69a819a8d089b1d9fd316eb5f154
Lê Thăng Long <long2323123@gmail.com>
2025-12-22 16:50:57 +0700

    push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.employees import router as employee_router
 from app.api.v1.companies import router as company_router
 from app.api.v1.folders import router as folder_router
@@ -8,7 +8,18 @@ app = FastAPI(
     title="Cenvi BE",
     version="1.0.0"
 )
+origins = [
+    "http://localhost:3000",           # React chạy ở máy local
+    "https://tools.cenvi.vn"  # Frontend khi deploy lên mạng (nếu có)
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # Hoặc dùng ["*"] để cho phép tất cả (không khuyến khích khi production)
+    allow_credentials=True,
+    allow_methods=["*"],   # Cho phép tất cả các method: GET, POST, PUT, DELETE...
+    allow_headers=["*"],   # Cho phép tất cả các header
+)
 # =========================
 # API v1
 # =========================
